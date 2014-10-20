93bcb77183bcd5ed24cf9b851dd5ffd1d0fdd76b
heyitsvincent <vincent@1-pps.com>
2014-10-20 15:51:44 -0500

    i did some stuff

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,3 +14,20 @@
 
 # Minus 5pts if your code has no comments
 # Minus 10pts if you only commit once to github
+from Tkinter import *
+root = Tk()
+
+# Create the canvas widget
+drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad.grid(row=0, column=1)
+
+# create_oval(x,y,width,height,fill color)
+oval = drawpad.create_oval(10, 50, 100, 100, fill='green')
+
+#create_square(top left x,top left y, bottom right x, bottom right y, fill color)
+square = drawpad.create_rectangle(300,200,400,300,fill='red')
+door = drawpad.create_rectangle(340,270,360,300,fill='brown')
+#create_line(top left x,top left y, bottom right x, bottom right y, fill color)
+line = drawpad.create_line(300,200,350,140)
+line2 = drawpad.create_line(400,200,350,140)
+root.mainloop()
